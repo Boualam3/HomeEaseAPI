144b1ca76b6ffb87f9d284ef35c3e432787cfea1
Boualam3 <boualam.bourahla@gmail.com>
2024-09-01 11:20:14 +0100

    add images router

--- properties/urls.py
+++ properties/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import PropertyImageViewSet
 
+
+router = routers.DefaultRouter()
+router.register(r'images', PropertyImageViewSet, basename='property-images')
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
