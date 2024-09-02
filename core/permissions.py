b41cdb6095a50c20db58a09c1a28a8f36c531032
Boualam3 <boualam.bourahla@gmail.com>
2024-09-02 12:15:48 +0100

    add permissions in profile viewset

---
+++ core/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+     allow only admin users (is_staff) to edit or delete an object.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
+
+
+class IsProfileOwner(permissions.BasePermission):
+    """
+    Object-level  allow owners of a profile object to edit or view it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # check if the profile's user is the same as the requesting user
+        return obj.user == request.user
--- core/views.py
+++ core/views.py
@@ -3,18 +3,37 @@ from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from core.models import Profile
 from core.serializers import ProfileSerializer
+from .permissions import IsProfileOwner
 
 
 class ProfileViewSet(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
-    # overrides the permissions in 'GET' and 'PUT' methods  in decorators below by adding another permission IsAuthenticated
-    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    # override permissions in profile/ endpoints
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            permission_classes = [
+                IsAuthenticated, IsProfileOwner | IsAdminUser
+            ]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [
+                IsAuthenticated, IsProfileOwner | IsAdminUser
+            ]
+        elif self.action == 'list':
+
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        # create instances of permissions coz its required when we override permissions
+        return [permission() for permission in permission_classes]
+
+    # overrides the permissions in 'GET' and 'PUT' methods  in decorators below by adding another permission IsAuthenticated and IsProfileOwner
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated, IsProfileOwner])
     def me(self, request):
         profile = Profile.objects.get(
             user_id=request.user.id)
