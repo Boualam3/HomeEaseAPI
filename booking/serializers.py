8e3548b2b52d01e8cf6fe5ea89f8ed38028816c6
Rashmi Agarwal <rashmiagar@gmail.com>
2024-09-11 19:35:19 +0530

    Added Booking and Order serializers

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,1 +1,18 @@
 from rest_framework import serializers
+from .models import Booking, Order
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'checkin', 'checkout', 'number_guests', 'status', 'property', 'profile']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        booking_id = self.context['booking_id']
+        return Order.objects.create(booking_id=booking_id, **validated_data)
+    
+    class Meta:
+        model = Order
+        fields = ['id', 'total_amount', 'vat', 'fees', 'discount', 'booking']
+    
