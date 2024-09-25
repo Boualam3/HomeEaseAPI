8f0894ff4fd15bbcf1bce7b0aa3f44e9ca365ba3
Boualam3 <boualam.bourahla@gmail.com>
2024-09-25 18:34:14 +0100

    merge the changes from booking_app_brh 'manage multi-environments with help of __init__.py file'

--- HomeEaseConfig/asgi.py
+++ HomeEaseConfig/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeEaseConfig.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeEaseConfig.settings")
 
 application = get_asgi_application()
